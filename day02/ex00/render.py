6dbf03b914783cefc621a649b00ff3668763e8f8
HelloThere-ImMat <dorr.mathias@gmail.com>
2025-10-21 15:28:26 +0200

    day02-ex00

--- day02/ex00/render.py
+++ day02/ex00/render.py
@@ -1,13 +1,24 @@
+import sys, os
 
 def func() :
     import re
     valuedic = {}
-
-    settings = open("settings.py")
+    if (sys.argv.__len__() != 2):
+        print("Wrong number of arguments")
+        return 1
+    settings = open('settings.py')
     settingsStr = settings.read()
 
     exec(settingsStr, {}, valuedic)
-    f = open("myCV.template")
+
+    if(not sys.argv[1].endswith('.template')):
+        print("Wrong file extension, should be .template")
+        return 1
+    if (not os.path.exists(sys.argv[1])):
+        print("File path not found")
+        return 1
+
+    f = open(sys.argv[1])
     file = f.read()
 
     def check_and_replace(match) :
@@ -18,7 +29,9 @@ def func() :
             return (match.group(0))
 
     string = re.sub("\\{(.*?)\\}", check_and_replace, file)
-    print(string)
+    with open("output.html", "w", encoding='utf-8') as file:
+        file.write(string)
+    return 0
 
 if __name__ == '__main__' : 
     func()
